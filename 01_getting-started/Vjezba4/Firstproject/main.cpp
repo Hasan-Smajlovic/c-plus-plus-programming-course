57020f79fbd8a4b946d7a996ee9c55aadfb2e0f3
Hasan Smajlovic <smajlovic.hasan02@gmail.com>
2019-07-06 18:39:21 +0200

    Cours practice:task3 and tast4

---
+++ 01_getting-started/Vjezba4/Firstproject/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main ()
+{ int favorite_number ;
+    std::cout <<"Type your favorite number between 1 and 100:" << std::endl;
+    std::cin>>favorite_number;
+    std::cout <<"Amaizing!That`s my favorite number too"<< std::endl;
+	return 0;
+}
