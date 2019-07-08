1f4dddd70c341bc8254845fc5a7d46ad15192b66
Hasan Smajlovic <smajlovic.hasan02@gmail.com>
2019-07-08 20:39:57 +0200

    Course_first_challenge

---
+++ 01_getting-started/Course_first_challenge/Course_first_challenge/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{ 
+    int favorite_number;
+std::cout <<"Enter your favorite number betwen 1 and 100:" ;
+std::cin >>favorite_number; 
+std::cout << "Amaizing!!Thats my favorite number too"<< std::endl;
+std::cout << "No really!!" <<(favorite_number)<<" is my favorite number!"<<std::endl;
+
+return 0;
+}
