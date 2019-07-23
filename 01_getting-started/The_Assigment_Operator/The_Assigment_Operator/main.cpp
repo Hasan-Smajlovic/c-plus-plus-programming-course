6ae4047a2453e6fa8e80e6f4c12c3b3bfd8ba983
Hasan Smajlovic <smajlovic.hasan02@gmail.com>
2019-07-23 15:45:20 +0200

    The_Assigment_Operator

---
+++ 01_getting-started/The_Assigment_Operator/The_Assigment_Operator/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+const int num1 {10};
+int num2 {20};
+
+100=num1;
+
+cout <<"num 1 is "<<num1<<endl;
+cout <<"num 2 is "<<num2<<endl;
+cout<<endl;
+return 0;
+}
